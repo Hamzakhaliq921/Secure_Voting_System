191c13a6c4fbcd752c779865072e3b75db4b05c1
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-28 20:33:52 -0600

    Login Form Logic Code

---
+++ securevotngsystem/forms/loginform.cs
@@ -0,0 +1,251 @@
+﻿using securevotngsystem.Singleton;
+using System;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace securevotngsystem.forms
+{
+    public partial class loginform : Form
+    {
+        public loginform()
+        {
+            InitializeComponent();
+
+            this.button1.Click += new System.EventHandler(this.btnLogin_Click);
+            this.button2.Click += new System.EventHandler(this.btnRegister_Click);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                errorProvider1.SetError(txtEmail, "Email is required");
+                return;
+            }
+
+            if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                errorProvider1.SetError(txtEmail, "Invalid email format");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                errorProvider1.SetError(txtPassword, "Password is required");
+                return;
+            }
+
+            try
+            {
+                var db = DbManager.Instance;
+                if (db.Connection.State != System.Data.ConnectionState.Open)
+                    db.Connection.Open();
+
+                using (var cmd = new SqlCommand("SELECT UserId, IsAdmin FROM Users WHERE Email = @Email AND PasswordHash = @PasswordHash", db.Connection))
+                {
+                    string hashedPassword = PasswordHelper.HashPassword(password);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@PasswordHash", hashedPassword);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int userId = reader.GetInt32(0);
+                            bool isAdmin = reader.GetBoolean(1);
+
+                            MessageBox.Show("Login successful!");
+
+                            if (isAdmin)
+                            {
+                                AdminPanelForm adminPanel = new AdminPanelForm();
+                                adminPanel.FormClosed += (s, args) => this.Close();
+                                adminPanel.Show();
+                            }
+                            else
+                            {
+                                VoteDashboardForm dashboard = new VoteDashboardForm(userId);
+                                dashboard.FormClosed += (s, args) => this.Close();
+                                dashboard.Show();
+                            }
+
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Account not found. Redirecting to registration...");
+                            this.Hide();
+                            new Registrationform().Show();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Login error: {ex.Message}");
+            }
+            finally
+            {
+                if (DbManager.Instance.Connection.State == System.Data.ConnectionState.Open)
+                    DbManager.Instance.Connection.Close();
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Registrationform().Show();
+        }
+
+        private void loginform_Load(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+
+        // Designer declarations
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button button1;
+        private Button button2;
+        private ErrorProvider errorProvider1;
+        private System.ComponentModel.IContainer components;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+
+            // panel1
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(654, 47);
+            this.panel1.TabIndex = 0;
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(219, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voting System";
+
+            // pictureBox1
+            this.pictureBox1.BackgroundImage = global::securevotngsystem.Properties.Resources._491_pakistan;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 143);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+
+            // label2 (Email)
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(251, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email";
+
+            // label3 (Password)
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(251, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+
+            // txtEmail
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtEmail.Location = new System.Drawing.Point(361, 106);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(174, 24);
+            this.txtEmail.TabIndex = 4;
+
+            // txtPassword
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtPassword.Location = new System.Drawing.Point(361, 165);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(174, 24);
+            this.txtPassword.TabIndex = 5;
+
+            // button1 (Login)
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(255, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+
+            // button2 (Register)
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(255, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Create account";
+            this.button2.UseVisualStyleBackColor = false;
+
+            // errorProvider1
+            this.errorProvider1.ContainerControl = this;
+
+            // loginform
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(654, 350);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "loginform";
+            this.Load += new System.EventHandler(this.loginform_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}

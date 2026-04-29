38d5d15f9174c9d70af4c9a60a9331c8f43f993a
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-29 09:31:35 -0600

    Registration form Design And Logic

---
+++ securevotngsystem/forms/Registrationform.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace securevotngsystem
+{
+    partial class Registrationform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registrationform));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLoginRedirect = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(275, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voting System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(278, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(278, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label4.Location = new System.Drawing.Point(278, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirm Password";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(23, 94);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(167, 143);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 143);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(454, 114);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(173, 24);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(454, 161);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(173, 24);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(454, 204);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.PasswordChar = '*';
+            this.txtConfirmPassword.Size = new System.Drawing.Size(173, 24);
+            this.txtConfirmPassword.TabIndex = 7;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnRegister.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(282, 252);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(345, 40);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLoginRedirect
+            // 
+            this.btnLoginRedirect.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnLoginRedirect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoginRedirect.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoginRedirect.ForeColor = System.Drawing.Color.White;
+            this.btnLoginRedirect.Location = new System.Drawing.Point(282, 298);
+            this.btnLoginRedirect.Name = "btnLoginRedirect";
+            this.btnLoginRedirect.Size = new System.Drawing.Size(345, 35);
+            this.btnLoginRedirect.TabIndex = 9;
+            this.btnLoginRedirect.Text = "Already Registered !";
+            this.btnLoginRedirect.UseVisualStyleBackColor = false;
+            this.btnLoginRedirect.Click += new System.EventHandler(this.btnLoginRedirect_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Registrationform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoginRedirect);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Registrationform";
+            this.Text = "Registration form";
+            this.Load += new System.EventHandler(this.Registrationform_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLoginRedirect;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
---
+++ securevotngsystem/forms/Registrationform.cs
@@ -0,0 +1,123 @@
+﻿using securevotngsystem.forms;
+using securevotngsystem.Singleton;
+using System;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace securevotngsystem
+{
+    public partial class Registrationform : Form
+    {
+        public Registrationform()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            errorProvider.Clear();
+
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
+
+            // Email validation
+            if (string.IsNullOrWhiteSpace(email) || email == "Enter your email")
+            {
+                errorProvider.SetError(txtEmail, "Email is required");
+                return;
+            }
+            else if (!Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                errorProvider.SetError(txtEmail, "Invalid email format");
+                return;
+            }
+
+            // Password validation
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                errorProvider.SetError(txtPassword, "Password is required");
+                return;
+            }
+
+            // Confirm password validation
+            if (password != confirmPassword)
+            {
+                errorProvider.SetError(txtConfirmPassword, "Passwords do not match");
+                return;
+            }
+
+            try
+            {
+                SqlConnection conn = DbManager.Instance.Connection;
+
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                // Check if email already exists
+                using (var checkEmailCmd = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Email = @Email", conn))
+                {
+                    checkEmailCmd.Parameters.AddWithValue("@Email", email);
+                    int count = (int)checkEmailCmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        errorProvider.SetError(txtEmail, "Email already registered");
+                        MessageBox.Show("Email is already registered. Redirecting to login...");
+
+                        this.Hide();
+                        new loginform().Show();
+                        return;  // Stop registration here
+                    }
+                }
+
+                // Insert new user if email is unique
+                using (var cmd = new SqlCommand("INSERT INTO Users (Email, PasswordHash) VALUES (@Email, @PasswordHash)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    string hashedPassword = PasswordHelper.HashPassword(password);
+                    cmd.Parameters.AddWithValue("@PasswordHash", hashedPassword);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Registration successful!");
+
+                this.Hide(); // Hide registration form
+                new loginform().Show(); // Show login form
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627) // Unique constraint violation (if DB constraint exists)
+                {
+                    errorProvider.SetError(txtEmail, "Email already registered");
+                    MessageBox.Show("Email is already registered. Redirecting to login...");
+
+                    this.Hide();
+                    new loginform().Show();
+                }
+                else
+                {
+                    MessageBox.Show($"Database Error: {ex.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unexpected Error: {ex.Message}");
+            }
+        }
+
+        private void btnLoginRedirect_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new loginform().Show();
+        }
+
+        private void Registrationform_Load(object sender, EventArgs e)
+        {
+            // Optional: Any initialization code here
+        }
+    }
+}

bc050308dc58b4a81972fcdecc865a72d1a95e4e
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-29 09:40:02 -0600

    ChainOfResponsibilityVoteStrategy File

---
+++ securevotngsystem/strategy/ChainOfResponsibilityVoteStrategy.cs
@@ -0,0 +1,19 @@
+﻿public class ChainOfResponsibilityVoteStrategy : IVoteStrategy
+{
+    public void Vote(int userId, int candidateId)
+    {
+        string electionType = "General"; // or "Local", decide based on your use case
+
+        // Create the chain of handlers
+        var checkHandler = new CheckAlreadyVotedHandler();
+        var castHandler = new CastVoteHandler();
+        var updateHandler = new UpdateUserVoteStatusHandler();
+
+        // Link the handlers together in order
+        checkHandler.SetNext(castHandler);
+        castHandler.SetNext(updateHandler);
+
+        // Start the chain from the first handler
+        checkHandler.Handle(userId, candidateId, electionType);
+    }
+}

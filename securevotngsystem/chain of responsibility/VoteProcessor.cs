5ba2dee15e4c33c8a8ffc27040a14fdaae9a1b44
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-28 18:55:14 -0600

    Vote Processor Chain Of Responsibility Pattern Code

---
+++ securevotngsystem/chain of responsibility/VoteProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace securevotngsystem
+{
+    public class VoteProcessor
+    {
+        private readonly VoteHandler _chain;
+
+        public VoteProcessor()
+        {
+            // Setup the chain: Check -> Cast -> Update
+            var checkHandler = new CheckAlreadyVotedHandler();
+            var castHandler = new CastVoteHandler();
+            var updateHandler = new UpdateUserVoteStatusHandler();
+
+            checkHandler.SetNext(castHandler);
+            castHandler.SetNext(updateHandler);
+
+            _chain = checkHandler;
+        }
+
+        public void Process(int userId, int candidateId, string electionType)
+        {
+            _chain.Handle(userId, candidateId, electionType);
+        }
+    }
+}

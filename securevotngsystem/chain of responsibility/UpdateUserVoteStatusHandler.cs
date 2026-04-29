1fcfedfdf36c98f5981ed4f1fcddaf4a679c3b10
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-28 18:53:32 -0600

    Update User Vote Status Handler File Chain Of Responsibility

---
+++ securevotngsystem/chain of responsibility/UpdateUserVoteStatusHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace securevotngsystem
+{
+    public class UpdateUserVoteStatusHandler : VoteHandler
+    {
+        public override void Handle(int userId, int candidateId, string electionType)
+        {
+            var db = DbManager.Instance;
+
+            string columnToUpdate = electionType == "Local" ? "HasVotedLocal" : "HasVotedGeneral";
+            var cmd = new SqlCommand($"UPDATE Users SET {columnToUpdate} = 1 WHERE UserId = @userId", db.Connection);
+            cmd.Parameters.AddWithValue("@userId", userId);
+
+            db.Connection.Open();
+            cmd.ExecuteNonQuery();
+            db.Connection.Close();
+
+            _next?.Handle(userId, candidateId, electionType);
+        }
+    }
+}

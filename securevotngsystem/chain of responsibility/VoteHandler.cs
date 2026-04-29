fb6435b09fd5b1fe4ef106652064d86ed2690a45
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-28 18:54:44 -0600

    Vote Handler Chain Of Responsibility Pattern Code

---
+++ securevotngsystem/chain of responsibility/VoteHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace securevotngsystem
+{
+    public abstract class VoteHandler
+    {
+        protected VoteHandler _next;
+
+        public void SetNext(VoteHandler next)
+        {
+            _next = next;
+        }
+
+        public abstract void Handle(int userId, int candidateId, string electionType);
+    }
+}

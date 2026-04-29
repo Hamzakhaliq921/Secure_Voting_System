d5fd55c90d9c75a143aed5524648e5d5a87192dd
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-29 09:40:41 -0600

    IVoteStrategy Interface file

---
+++ securevotngsystem/strategy/IVoteStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace securevotngsystem
+{
+    public interface IVoteStrategy
+    {
+        void Vote(int userId, int candidateId);
+    }
+}

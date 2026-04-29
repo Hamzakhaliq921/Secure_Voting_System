277af1d513e5392102c2bd0cb97cce52a8da129a
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-29 09:39:08 -0600

    Database Manager Class File

---
+++ securevotngsystem/Singleton/DbManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace securevotngsystem
+{
+    public sealed class DbManager
+    {
+        private static DbManager _instance;
+        private static readonly object _lock = new object();
+        public SqlConnection Connection { get; }
+
+        private DbManager()
+        {
+            Connection = new SqlConnection("Data Source=DESKTOP-D6FIOOH\\SQLEXPRESS01;Initial Catalog=securevotingsystem;Integrated Security=True;");
+        }
+
+        public static DbManager Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instance ?? (_instance = new DbManager());
+                }
+            }
+        }
+    }
+}

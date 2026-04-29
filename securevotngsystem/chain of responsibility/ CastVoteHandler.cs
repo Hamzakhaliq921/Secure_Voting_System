9c67edd89d2a5d7a37b6a79711006b705a193f1f
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-28 18:52:01 -0600

    Cast Vote Handler file logic

---
+++ securevotngsystem/chain of responsibility/ CastVoteHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+namespace securevotngsystem
+{
+    public class CastVoteHandler : VoteHandler
+    {
+        public override void Handle(int userId, int candidateId, string electionType)
+        {
+            var db = DbManager.Instance;
+
+            db.Connection.Open();
+            var cmd = new SqlCommand(
+                "INSERT INTO Votes (UserId, CandidateId, ElectionType) VALUES (@userId, @candidateId, @type)", db.Connection);
+            cmd.Parameters.AddWithValue("@userId", userId);
+            cmd.Parameters.AddWithValue("@candidateId", candidateId);
+            cmd.Parameters.AddWithValue("@type", electionType);
+            cmd.ExecuteNonQuery();
+            db.Connection.Close();
+
+            _next?.Handle(userId, candidateId, electionType);
+        }
+    }
+}
